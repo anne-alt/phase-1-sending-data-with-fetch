8eba5d4868210f1940ea776327cce2451370f74c
Anne Ogongo <anneogongo98@gmail.com>
2022-09-05 09:51:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const body = document.querySelector('body');
+
+function submitData(userName, userEmail) {
+  let users = {
+        name: userName,
+        email: userEmail
+    }
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+        "Content-Type" : "application/json",
+        Accept: "application/json"
+    },
+    body : JSON.stringify(users)
+  })
+  .then(response => response.json())
+  .then((user) => {body.append(id)})
+  .catch(error => {
+    alert("unauthorised User!");
+    body.append(error);
+  })
+}
+submitData();
